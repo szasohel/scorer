6d429f6694b1793f5f57e20bcec36f81762cada1
Sohel <Sayed.X.Sohel@questdiagnostics.com>
2019-05-15 16:05:30 -0400

    environment update for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCIzWvibVMl9G6uclbKcmnoSJ4LVf1SfZ8',
+    authDomain: 'scorer-56f42.firebaseapp.com',
+    databaseURL: 'https://scorer-56f42.firebaseio.com',
+    projectId: 'scorer-56f42',
+    storageBucket: 'scorer-56f42.appspot.com',
+    messagingSenderId: '505046071609',
+    appId: '1:505046071609:web:1b8e616ab82f7f1a'
+  }
 };
