c60a0486c8fc7cc251570cfea3cc05a18b4d90c9
Sohel <Sayed.X.Sohel@questdiagnostics.com>
2019-05-31 17:29:08 -0400

    add marquee

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from '../services/auth-service.service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +12,21 @@ import { AuthServiceService } from '../services/auth-service.service';
 export class HomeComponent implements OnInit {
   showbutton: boolean;
   error: string;
+  subscription: Subscription;
+  announcement = '';
 
-  constructor(private authService: AuthServiceService) { }
+
+  constructor(private authService: AuthServiceService, private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.subscription = this.db
+      .list('/data/announcement')
+      .valueChanges().pipe(
+        tap((res: any) => {
+          console.log(res);
+          this.announcement = res[0];
+        })
+      ).subscribe();
     this.authService.isAuthenticated$.subscribe((isAuthed) => {
       this.showbutton = isAuthed;
       this.error = this.authService.error;
